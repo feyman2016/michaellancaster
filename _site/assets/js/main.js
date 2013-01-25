3f26ce53704bfabdad949b2f6640284134b08802
Michael Lancaster <Michael.Lancaster@chighi-nmx4310.golinharris.com>
2013-01-25 15:36:03 -0600

    new content, style and js updates

---
+++ _site/assets/js/main.js
@@ -0,0 +1,37 @@
+//js main
+function showArticles() {
+	var bt = $('#bt-show-articles'),
+		elAside = $('#aside'),
+		elArticles = $('#articles-list'),
+		elContent = $('#content'),
+		active = 'active';
+
+	bt.on('click', function(e) {
+		e.preventDefault();
+
+		if( !$(this).hasClass(active) ) {
+			elAside.addClass(active);
+
+			$(this).addClass(active);
+
+			elArticles.removeClass('vh');
+			elArticles.addClass(active);
+			
+			elContent.addClass(active);
+		} else {
+			elAside.removeClass(active);
+
+			$(this).removeClass(active);
+
+			elArticles.removeClass(active);
+			elArticles.delay(500).addClass('vh');
+			
+			elContent.removeClass(active);
+		}
+	});
+}
+
+
+$(function () {
+	showArticles();
+});
