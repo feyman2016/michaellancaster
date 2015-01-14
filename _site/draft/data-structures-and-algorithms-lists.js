33fc2ce4adff869d0b931052898a7987c177141f
Michael Lancaster <mlancaster@mlancaster-mbp.duncllc.com>
2015-01-14 10:12:45 -0600

    add new pic and update lab page copy

--- _site/draft/data-structures-and-algorithms-lists.js
+++ _site/draft/data-structures-and-algorithms-lists.js
@@ -1,50 +1,3 @@
----
-layout: post
-title: Ep.3 Data Structures and Algorithms with JS - Lists
-
-excerpt: Let's get start and see how data structures can help us design better software solutions...today I'm going to talk briefly about Lists
----
-
-<div class="intro-series">
-    <strong>Be advised!</strong> <br> 
-    This post is part of the series: Data Structures and Algorithms with JS therefore before continue to read I suggest you <a href="/blog/javascript-data-structure-algorithms-series-ep1/" title="Data Structures and Algorithms Ep.1">the introduction</a> to this series of posts if you haven't read yet. thanks.
-</div>
-
-Let's get start and see how data structures can help us design better software solutions...today I'm going to talk briefly about Lists
-
-### Lists
-
-Answer out and loud what are lists? ... Yes that's it. We all intuitively understand what lists are but to be more clear Lists are simply a collection of items therefore a To-Do list, Grocery shop list, book list, Phone number list and so on.
-
-In computer programing Lists are under the ADT (Abstract Data Type) category and can have order/or not (each element has your position) where the element can be duplicated or there's no restrictions on different data types but usually assumed to have the same data type so lists are useful when search and sorted order **isn't** in need which otherwise would perform a slow and complex data structure design.
-
-A list is said to be `empty` when it contains no elements. The number of elements currently stored is called the `length` of the list. The beginning of the list is called the `head`, the end of the list is called the `tail`.
-
-The basic operations we want on a list is to increase and decrease in size adding and removing new elements..We also want to add or remove elements on any position of the list.. access the element value either to just read or change it.. Access the next or prev based on the current element.. we also want to be able to create new lists or clear one.
-
-So here's our actions.
-
-- `position` current position in the list where any action will take place
-- `currPos` return the position of the current element
-- `clear` remove all elements of the list
-- `insertAfter` insert an element after target specified in the list
-- `insertBefore` insert an element before target specified in the list
-- `append` an element to the end of the list
-- `remove` an element from the list
-- `length` number of elements in the list
-- `find` an element in the list and return the position of the element
-- `contains` an element in the list return boolean
-- `head` set current position to first element in the list
-- `tail` set current position to last element in the list
-- `prev` set the current position one step left of the current position, no change if already at beginning
-- `next` set the current position one step right of the current position, no change if already at end
-- `moveTo` move current position to specified position
-- `to String` return string representation of the list
-- `getElement` return element of the current position in the list
-
-That being said lets get our hands dirty.
-
-<pre><code data-language="javascript">
 /**
  * List constructor
  * @class List
@@ -148,17 +101,17 @@ List.prototype.clear = function () {
 
 /**
  * Move current position to the front/first in the list
- * @method head
+ * @method front
  */
-List.prototype.head = function () {
+List.prototype.front = function () {
     this.position = 0;
 };
 
 /**
  * Move current position to the end in the list
- * @method tail
+ * @method end
  */
-List.prototype.tail = function () {
+List.prototype.end = function () {
     this.position = this.listSize;
 };
 
@@ -174,9 +127,9 @@ List.prototype.prev = function () {
 };
 
 /**
- * Set current position on step right in the list
- * if not at the end/tail element
- * @method next
+ * Set current position on step left/back in the list
+ * if not at the first element
+ * @method prev
  */
 List.prototype.next = function () {
     if ( this.position !== (this.listSize - 1) ) {
@@ -275,19 +228,14 @@ List.prototype.debug = function (options) {
         console.log('List size >>', this.listSize );
     }
     if ( options.contains ) {
-        console.log('Contains element >>', this.contains(options.contains) );
+        console.log('Contains element', this.contains(options.contains) );
     }
     if ( options.isEmpty ) {
-        console.log('is empty >>', this.isEmpty() );
+        console.log('is empty', this.isEmpty() );
     }
 }
-</code></pre>
 
-the code example above show all methods and properties our list need to have and I also added a method call `debug` so we can do some simple debug/log.
 
-Let's create a list of movies and at the end lets use our simple test method
-
-<pre><code data-language="javascript">
 /**
 * Movie list
 */
@@ -302,10 +250,10 @@ movies.append('G.I Joe');
 movies.append('Interstellar');
 
 // move action
-movies.tail();
+movies.end();
 movies.prev();
 movies.moveTo(4);
-movies.head();
+movies.front();
 
 // delete action
 movies.remove('G.I Joe');
@@ -324,8 +272,5 @@ movies.debug({
     contains: 'Batman',
     isEmpty: true
 });
-</code></pre>
 
-And this is was the Ep.3 about Data Structures and Algorithms - Lists.
 
-Cheers!
